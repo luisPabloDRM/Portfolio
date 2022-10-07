1921168670428f9aad9e2c40b6c3bbe65166f522
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-10-07 16:18:27 +0200

    FEAT: Mobile First in HOmepage component2.1

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -16,14 +16,19 @@ export const Homepage = () => {
 
     <div className='container'>
         <span>L</span>
+        <span>U</span>
+        <span>I</span>
+        <span>S</span>
+        <span>&nbsp;</span>
         <span>P</span>
-        <span>D</span>
-        <span>R</span>
-        <span>M</span>
+        <span>A</span>
+        <span>B</span>
+        <span>L</span>
+        <span>O</span>
 
     </div>
     <div>
-        <NavLink to = '/main'><button>Mi PortFolio</button></NavLink>
+        <NavLink to = '/main'><button>Click to know me</button></NavLink>
     </div>
 
 
