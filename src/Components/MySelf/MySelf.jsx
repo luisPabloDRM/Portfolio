c5932f6e715c19f90390cd4b81b58715768dce2d
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-10-14 12:23:06 +0200

    FEAT:Adding some responsive styles2.1

--- src/Components/MySelf/MySelf.jsx
+++ src/Components/MySelf/MySelf.jsx
@@ -3,8 +3,9 @@ import './MySelf.scss'
 
 export const MySelf = () => {
   return (
-<div className='gridContainer_1'>
-
+    <>
+    <h1  className='big_title' >About me</h1>
+    <div className='gridContainer_1'>
 <div  >
         <img src="https://cdn.discordapp.com/attachments/965645891820212318/1029070976522133574/Captura_de_Pantalla_2022-10-10_a_las_18.40.13.png" alt="about me" />
     </div>
@@ -31,6 +32,8 @@ export const MySelf = () => {
 
     </div>
 </div>
+    </>
+
 
     
   )
