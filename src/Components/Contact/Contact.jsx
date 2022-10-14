0876db2caf457c91ff7f8c2e7483549e6aced866
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-10-14 17:32:09 +0200

    FEAT: Some responsive changues

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faMobile,  } from '@fortawesome/free-solid-svg-icons';
-
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import './Contact.scss'
 
 const Contact = () => {
@@ -12,8 +12,10 @@ const Contact = () => {
         <h3>Luis Pablo  Dev</h3>
         <p>This is a portfolio that is made with the same measure of illusion, motivation, humility and desire to continue learning..</p>
         <ul class="socials">
-            <li><a href="#"> <FontAwesomeIcon icon={faEnvelope} /></a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faMobile} /></a></li>
+            <li> <p><FontAwesomeIcon icon={faEnvelope} />lp.delriom@gmail.com</p></li>
+            <li><p ><FontAwesomeIcon icon={faMobile} />685148169</p></li>
+            <li><a href='https://github.com/luisPabloDRM' ><FontAwesomeIcon icon={faGithub} /></a></li>
+            <li><a href='https://www.linkedin.com/in/lpdrm/'><FontAwesomeIcon icon={faLinkedin} /></a></li>
             
         </ul>
     </div>
