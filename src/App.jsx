768ce6449c7535d88660992ca60a501a576e8af5
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-10-14 17:24:23 +0200

    FEAT:Adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Homepage } from './Components/HomePage/Homepage';
 import Principal from './Pages/Principal/Principal';
+import { MySelf } from './Components/MySelf/MySelf';
+import Education from './Components/Education/Education';
+import Skills from './Components/Skills/Skills';
+import Projects from './Components/Projects/Projects';
 
 
 function App() {
@@ -16,6 +20,10 @@ function App() {
     <Routes>
       <Route path='/' element = {<Homepage/>} /> 
       <Route path='/main' element = {<Principal/>} /> 
+      <Route path='/about' element = {<MySelf/>} /> 
+      <Route path='/education' element = {<Education/>} /> 
+      <Route path='/skills' element = {<Skills/>} /> 
+      <Route path='/projects' element = {<Projects/>} /> 
     </Routes>
     </>
    
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faMobile,  } from '@fortawesome/free-solid-svg-icons';
 
 import './Contact.scss'
 
 const Contact = () => {
   return (
-    <div className='container_ed'>
-       
-        <h1>Contact</h1>
-       
+    <footer>
+    <div class="footer-content">
+        <h3>Luis Pablo  Dev</h3>
+        <p>This is a portfolio that is made with the same measure of illusion, motivation, humility and desire to continue learning..</p>
+        <ul class="socials">
+            <li><a href="#"> <FontAwesomeIcon icon={faEnvelope} /></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faMobile} /></a></li>
+            
+        </ul>
     </div>
-  )
-}
+    <div class="footer-bottom">
+        <p>copyright &copy; LPDRM  </p>
+                <div class="footer-menu">
+                  <ul class="f-menu">
+                    <li><NavLink to = '/about'>About Me</NavLink></li>
+                    <li><NavLink to = '/skills'>Skills</NavLink></li>
+                    <li><NavLink to = '/projects'>Projects</NavLink></li>
+                    <li><NavLink to = '/'>Home</NavLink></li>
+                    
+                  </ul>
+                </div>
+    </div>
+
+</footer>
+  );
+};
 
 export default Contact
